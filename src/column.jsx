f67ac1382651b3eedb98dae6da18f74a2fb2e101
David Dean <vermicious.knid.d@gmail.com>
2020-01-25 10:11:24 -0700

    9. Move Items between Columns with react-beautiful-dnd using onDragEnd.

--- src/column.jsx
+++ src/column.jsx
@@ -7,6 +7,9 @@ const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
   border-radius: 2px;
+  width: 200px;
+  display: flex;
+  flex-direction: column;
 `
 const Title = styled.h3`
   padding: 8px;
@@ -15,6 +18,8 @@ const TaskList = styled.div`
   padding: 8px;
   transition: background-color 0.2s ease;
   background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')};
+  flex-grow: 1;
+  min-height: 100px;
 `
 
 class Column extends React.Component {
