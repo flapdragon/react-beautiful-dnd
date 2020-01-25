8cfc5a48cc6acc89302064b3e316e79274d42985
David Dean <vermicious.knid.d@gmail.com>
2020-01-25 12:59:13 -0700

    11. Create Reorderable Horizontal Lists with react-beautiful-dnd direction prop. Could not get styled components &:focus to work.

--- src/column.jsx
+++ src/column.jsx
@@ -7,7 +7,6 @@ const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
   border-radius: 2px;
-  width: 200px;
   display: flex;
   flex-direction: column;
 `
@@ -18,8 +17,8 @@ const TaskList = styled.div`
   padding: 8px;
   transition: background-color 0.2s ease;
   background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')};
-  flex-grow: 1;
-  min-height: 100px;
+
+  display: flex;
 `
 
 class Column extends React.Component {
@@ -30,6 +29,7 @@ class Column extends React.Component {
         <Droppable
           droppableId={this.props.column.id}
           isDropDisabled={this.props.isDropDisabled}
+          direction="horizontal"
         >
           {(provided, snapshot) => (
             <TaskList
--- src/task.jsx
+++ src/task.jsx
@@ -2,22 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 import { Draggable } from 'react-beautiful-dnd'
 
+// focus does not seem to work. neither my code or the code from the course, which i copy/pasted just to be safe.
 const Container = styled.div`
-  border: 1px solid lightgrey;
-  border-radius: 2px;
+  border: 3px solid lightgrey;
+  border-radius: 50%;
   padding: 8px;
-  margin-bottom: 8px;
-  background-color: ${props =>
-    props.isDragDisabled
-      ? 'lightgrey'
-      : props.isDragging
-        ? 'lightgreen'
-        : 'white'};
+  margin-right: 8px;
+  background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
+  width: 40px;
+  height: 40px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:focus {
+    outline: none;
+    border-color: red !important;
+  }
 `
 
 class Task extends React.Component {
   render() {
-    const isDragDisabled = this.props.task.id === 'task-1'
+    const isDragDisabled = false
     return (
       <Draggable
         draggableId={this.props.task.id}
@@ -32,7 +39,7 @@ class Task extends React.Component {
             isDragging={snapshot.isDragging}
             isDragDisabled={isDragDisabled}
           >
-            {this.props.task.content}</Container>
+            {this.props.task.content[0]}</Container>
         )}
       </Draggable>
     )
